777579bfb1d787ce28dedfb856f5b6f3b1e012ad
adaldean <adal.domingo13@outlook.com>
2026-05-10 15:41:09 -0600

    Accesibilidad y validacion de correo 4

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,7 +77,7 @@ if DATABASE_URL:
         'default': dj_database_url.config(
             default=DATABASE_URL,
             conn_max_age=600,
-            ssl_require=False
+            ssl_require=True
         )
     }
 else:
