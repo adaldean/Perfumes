23eb020e550b6172e23b4e40e48e7f319a558a77
J0seandres <josea0476@gmail.com>
2026-05-11 20:55:58 -0600

    fix(storage): use explicit s3 storages backend for media uploads

--- myproject/settings.py
+++ myproject/settings.py
@@ -131,18 +131,32 @@ if not DEBUG:
         AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
         AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
         AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
+        AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
         AWS_QUERYSTRING_AUTH = False
         AWS_S3_FILE_OVERWRITE = False
         AWS_DEFAULT_ACL = None
 
-        DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-        STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+        STORAGES = {
+            'default': {
+                'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
+            },
+            'staticfiles': {
+                'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+            },
+        }
 
-        STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
-        MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
+        STATIC_URL = '/static/'
+        MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
     else:
         # Si no hay S3, WhiteNoise maneja estáticos; media se queda local (limitado en Render)
-        STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+        STORAGES = {
+            'default': {
+                'BACKEND': 'django.core.files.storage.FileSystemStorage',
+            },
+            'staticfiles': {
+                'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+            },
+        }
 
     # Logging para producción (Render)
     LOGGING = {
